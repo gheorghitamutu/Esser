d6ed22efff2d8785bef12c298b8693752cb4c8ef
marcupalii <marcupalii@users.noreply.github.com>
2018-06-06 16:50:26 +0300

    Fixed sidebar

--- public/resources/js/user-nav-bar.js
+++ public/resources/js/user-nav-bar.js
@@ -3,90 +3,113 @@ const sidebarTrigger = document.querySelector('#sidebar-trigger');
 /* --------------------------------- Make room for the sidebar to appear on left side width 21 % -------------------------------------------- */
 
 function openLeftSideNavigationBar() {
-    document.getElementById("offCanvasSidenav").style.width = "21%";
-    document.getElementById("main").style.marginLeft = "21%";
-    document.getElementById("main").style.width = "79%";
-    document.getElementById("footer-container").style.marginLeft = "21%";
-    document.getElementById("sidebar-trigger").style.marginLeft = "14%";
-    document.getElementById("sidebar-trigger-container").style.marginLeft = "14%";
-    document.getElementById("sidebar-trigger-container").style.width = "87%";
+        document.getElementById("offCanvasSidenav").style.display = "flex";
+        document.getElementById("offCanvasSidenav").style.width = "21%";
 
-    // shadow the background
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-    /*
-    if(document.getElementById("body").clientWidth >=600){
-        closeLeftSideNavigationBar();
-        openLeftSideNavigationBarDisplayAllWidth();
-    }*/
+        document.getElementById("main").style.display = "flex";
+        document.getElementById("main").style.marginLeft = "21%";
+        document.getElementById("main").style.width = "79%";
+
+        document.getElementById("footer-container").style.marginLeft = "21%";
+        document.getElementById("sidebar-trigger").style.marginLeft = "14%";
+        document.getElementById("sidebar-trigger-container").style.marginLeft = "14%";
+        document.getElementById("sidebar-trigger-container").style.width = "87%";
+
+        // shadow the background
+        document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+
 }
 
 
 /* --------------------------------- Close sidebar reset changes -------------------------------------------- */
 function closeLeftSideNavigationBar() {
 
-    document.getElementById("offCanvasSidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0px";
-    document.getElementById("main").style.width = "100%";
-    document.getElementById("footer-container").style.marginLeft = "0";
-    document.getElementById("sidebar-trigger").style.marginLeft = "0";
-    document.getElementById("sidebar-trigger-container").style.marginLeft = "0";
-    document.getElementById("sidebar-trigger-container").style.width = "100%";
-    document.body.style.backgroundColor = "white";
+        document.getElementById("offCanvasSidenav").style.width = "0";
+        document.getElementById("offCanvasSidenav").style.display = "none";
+
+        document.getElementById("main").style.display = "flex";
+        document.getElementById("main").style.width = "100%";
+        document.getElementById("main").style.marginLeft = "0";
+
+        document.getElementById("footer-container").style.marginLeft = "0";
+        document.getElementById("sidebar-trigger").style.marginLeft = "0";
+        document.getElementById("sidebar-trigger-container").style.marginLeft = "0";
+        document.getElementById("sidebar-trigger-container").style.width = "100%";
+        document.body.style.backgroundColor = "white";
+
+
+
+    if(document.getElementById("body").clientWidth <=900) {
+        var sidenav_items = document.getElementsByClassName("sidenav-item");
+        for (let sidenav_item of sidenav_items) {
+            sidenav_item.style.marginLeft = "2%";
+            sidenav_item.style.marginRight = "2%";
+            sidenav_item.style.width = "96%";
+        }
+    }
+    if(document.getElementById("body").clientWidth >=900) {
+        var sidenav_items = document.getElementsByClassName("sidenav-item");
+        for (let sidenav_item of sidenav_items) {
+            sidenav_item.style.marginLeft = "15%";
+            sidenav_item.style.marginRight = "15%";
+            sidenav_item.style.width = "70%";
+        }
+    }
+
 }
 
 
 /* --------------------------------- Make sidebar full scren , hide every thing else  -------------------------------------------- */
 
 function openLeftSideNavigationBarDisplayAllWidth() {
-    document.getElementById("main").style.display = "none";
-    document.getElementById("offCanvasSidenav").style.width="100%";
-    var sidenav_items=document.getElementsByClassName("sidenav-item");
-    for(sidenav_item of sidenav_items){
-        sidenav_item.style.marginLeft="15%";
-        sidenav_item.style.marginRight="15%";
-        sidenav_item.style.width="70%";
+    if(document.getElementById("body").clientWidth <600) {
+
+        document.getElementById("main").style.width = "0";
+        document.getElementById("main").style.display = "none";
+
+        document.getElementById("offCanvasSidenav").style.display = "flex";
+        document.getElementById("offCanvasSidenav").style.width = "100%";
+
+        var sidenav_items = document.getElementsByClassName("sidenav-item");
+        for (let sidenav_item of sidenav_items) {
+            sidenav_item.style.marginLeft = "15%";
+            sidenav_item.style.marginRight = "15%";
+            sidenav_item.style.width = "70%";
+        }
     }
-    /*
-    if(document.getElementById("body").clientWidth >=600){
-        closeLeftSideNavigationBarDisplayAllWidth();
-        openLeftSideNavigationBar();
-    }*/
 }
 
-/* --------------------------------- Reset changes, close full screen sidebar -------------------------------------------- */
-function closeLeftSideNavigationBarDisplayAllWidth() {
-    document.getElementById("main").style.display = "flex";
-    document.getElementById("main").style.width = "100%";
-    document.getElementById("offCanvasSidenav").style.width = "0";
-    document.getElementById("footer-container").style.marginLeft = "0";
-    document.getElementById("sidebar-trigger").style.marginLeft = "0";
-    document.getElementById("sidebar-trigger-container").style.marginLeft = "0";
-    document.getElementById("sidebar-trigger-container").style.width = "100%";
-    document.body.style.backgroundColor = "white";
-}
 
 /* -------------------------------- Control the sidebar trigger ----------------------------------------------------- */
 
+var active=0;
 sidebarTrigger.addEventListener('click', function() {
-    if(document.getElementById("body").clientWidth >=600){
+
         if (this.classList.contains('active')){
-            this.classList.remove('active')
+            this.classList.remove('active');
+            active=0;
             closeLeftSideNavigationBar();
         }else{
-            this.classList.add('active')
-            openLeftSideNavigationBar();
+            this.classList.add('active');
+            active=1;
+            if(document.getElementById("body").clientWidth >=600) {
+                openLeftSideNavigationBar();
+            }else{
+                openLeftSideNavigationBarDisplayAllWidth();
+            }
         }
-    }else{
-        if (this.classList.contains('active')) {
-            this.classList.remove('active')
-            closeLeftSideNavigationBarDisplayAllWidth();
-        }else {
-            this.classList.add('active')
+});
+
+/*----------------------------------    while sidebar is active this check for width to change from fullscreen to left side bar--------------*/
+var body =document.getElementById("body");
+window.addEventListener('resize',function(){
+    if( active == 1 ){
+        if (body.clientWidth >= 600) {
+            closeLeftSideNavigationBar();
+            openLeftSideNavigationBar();
+        } else {
+            closeLeftSideNavigationBar();
             openLeftSideNavigationBarDisplayAllWidth();
         }
     }
-})
-
-
-
-
+});
