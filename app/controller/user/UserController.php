ddb6adcead1804e11ab02edbc22b7e6b3f160c67
Gheorghita Mutu <ghitamt@rocketmail.com>
2018-06-13 19:07:39 +0300

    UserController:     logs() -> added logs array sorted by created_at passed to the view ItemLogMapper:     extends AbstractMapper UserGroupLogMapper:     extends UserGroupLogMapper view/user/logs/logs/phtml:     if data array is empty -> default message else -> show each row

--- app/controller/user/UserController.php
+++ app/controller/user/UserController.php
@@ -76,9 +76,37 @@ class UserController extends Controller
 
     public function logs()
     {
+        $this->model('ItemGrouplog');
+
+        $item_group_logs = $this->model_class->get_mapper()->findAll(
+            $fields = false);
+
+        $this->model('Itemlog');
+
+        $item_logs = $this->model_class->get_mapper()->findAll(
+            $fields = false);
+
+        $this->model('UserGroupLog');
+
+        $user_group_logs = $this->model_class->get_mapper()->findAll(
+            $fields = false);
+
+        $logs = array_merge($item_group_logs, $item_logs, $user_group_logs);
+
+        usort($logs, function ($a, $b)
+        {
+            if ($a['uLogCreatedAt'] == $b['uLogCreatedAt'])
+            {
+                return 0;
+            }
+
+            return ($a['uLogCreatedAt'] < $b['uLogCreatedAt']) ? -1 : 1;
+        });
+
+
         View::CreateView(
             'user' . DIRECTORY_SEPARATOR . 'logs' . DIRECTORY_SEPARATOR . 'logs',
-            [],
+            ["logs" => $logs],
             'Logs area');
     }
 
--- app/models/mapper/UserGroupLogMapper.php
+++ app/models/mapper/UserGroupLogMapper.php
@@ -11,7 +11,7 @@ use AppModel\Useracc;
 use DatabaseConnectivity;
 
 
-class UserGroupLogMapper
+class UserGroupLogMapper extends AbstractMapper
 {
     protected $_entityTable = 'USERGROUPLOGS';
     protected $_entityClass = 'UserGroupLog';
