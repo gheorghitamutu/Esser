6d59caa6bb19bc2db4e6d2bbf37e6b8e3068f3ca
Bugeag George <bugeag.george@live.com>
2018-06-13 09:38:43 +0300

    Fixed model errors due to set[Parameters];

--- app/models/Itemgroupownership.php
+++ app/models/Itemgroupownership.php
@@ -22,26 +22,26 @@ class Itemgroupownership extends AbstractEntity
         return $this->mapper;
     }
 
-    public function setId($id  = false) {
-        if (!$id) {
-            $this->_values['iGOwnershipId'] = null;
-        }
-        else {
-            $this->_values['iGOwnershipId'] = null;
-        }
-    }
-
-    public function setOwnerId($id) {
-        if (!filter_var($id, FILTER_VALIDATE_INT, array('options' => array('min_range' => 1, 'max_range' => 999999999)))) {
-            throw new InvalidArgumentException('The owner user group id should be a int value between 1 and 999999999');
-        }
-        $this->_values['iGOwnershipId'] = $id;
-    }
-
-    public function setGrouId($id) {
-        if (!filter_var($id, FILTER_VALIDATE_INT, array('options' => array('min_range' => 1, 'max_range' => 999999999)))) {
-            throw new InvalidArgumentException('The owned item group id should be a int value between 1 and 999999999');
-        }
-        $this->_values['iGOwnershipId'] = $id;
-    }
+//    public function setId($id  = false) {
+//        if (!$id) {
+//            $this->_values['iGOwnershipId'] = null;
+//        }
+//        else {
+//            $this->_values['iGOwnershipId'] = null;
+//        }
+//    }
+//
+//    public function setOwnerId($id) {
+//        if (!filter_var($id, FILTER_VALIDATE_INT, array('options' => array('min_range' => 1, 'max_range' => 999999999)))) {
+//            throw new InvalidArgumentException('The owner user group id should be a int value between 1 and 999999999');
+//        }
+//        $this->_values['iGOwnershipId'] = $id;
+//    }
+//
+//    public function setGrouId($id) {
+//        if (!filter_var($id, FILTER_VALIDATE_INT, array('options' => array('min_range' => 1, 'max_range' => 999999999)))) {
+//            throw new InvalidArgumentException('The owned item group id should be a int value between 1 and 999999999');
+//        }
+//        $this->_values['iGOwnershipId'] = $id;
+//    }
 }
--- app/models/Usrntfrelation.php
+++ app/models/Usrntfrelation.php
@@ -23,26 +23,26 @@ class Usrntfrelation extends AbstractEntity
     }
 
 
-    public function setId($id = false) {
-        if (!$id) {
-            $this->_values['usrNRelationId'] = null;
-        }
-        else {
-            $this->_values['usrNRelationId'] = null;
-        }
-    }
-
-    public function setNotificationId($id) {
-        if (!filter_var($id, FILTER_VALIDATE_INT, array('options' => array('min_range' => 1, 'max_range' => 999999999)))) {
-            throw new InvalidArgumentException('Specified notification id for user-notification relation must be between 1 and 999999999!');
-        }
-        $this->_values['usrNNotificationId'] = $id;
-    }
-
-    public function setNotifiedId($id) {
-        if (!filter_var($id, FILTER_VALIDATE_INT, array('options' => array('min_range' => 1, 'max_range' => 999999999)))) {
-            throw new InvalidArgumentException('Specified item id for user-notification relation for this notification must be between 1 and 999999999!');
-        }
-        $this->_values['usrNNotifiedAccId'] = $id;
-    }
+//    public function setId($id = false) {
+//        if (!$id) {
+//            $this->_values['usrNRelationId'] = null;
+//        }
+//        else {
+//            $this->_values['usrNRelationId'] = null;
+//        }
+//    }
+//
+//    public function setNotificationId($id) {
+//        if (!filter_var($id, FILTER_VALIDATE_INT, array('options' => array('min_range' => 1, 'max_range' => 999999999)))) {
+//            throw new InvalidArgumentException('Specified notification id for user-notification relation must be between 1 and 999999999!');
+//        }
+//        $this->_values['usrNNotificationId'] = $id;
+//    }
+//
+//    public function setNotifiedId($id) {
+//        if (!filter_var($id, FILTER_VALIDATE_INT, array('options' => array('min_range' => 1, 'max_range' => 999999999)))) {
+//            throw new InvalidArgumentException('Specified item id for user-notification relation for this notification must be between 1 and 999999999!');
+//        }
+//        $this->_values['usrNNotifiedAccId'] = $id;
+//    }
 }
