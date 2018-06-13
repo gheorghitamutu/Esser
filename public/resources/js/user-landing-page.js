1d994758eb7adb3cdcd5b3d92f1c3d3551c30c56
marcupalii <marcupalii@users.noreply.github.com>
2018-06-13 23:11:58 +0300

    Fix overflow table bug

--- public/resources/js/user-landing-page.js
+++ public/resources/js/user-landing-page.js
@@ -19,43 +19,5 @@ function showHideAdminCpButton() {
     }
 }
 
-function adjustTable(){
-    /*
-    var table_products = document.getElementsByClassName("items-content-user-landing-page");
-    var table_users = document.getElementsByClassName("users-content-user-landing-page");
-    var height_table_products;
-    var height_table_users;
-
-    height_table_products=table_products[0].clientHeight;
-    height_table_users=table_users[0].clientHeight;
-
-    if(height_table_products> height_table_users ) {
-        table_products[0].setAttribute("style", "height:" + height_table_products + "px");
-    }else{
-        table_users[0].setAttribute("style", "height:" + height_table_users + "px");
-    }
-
-    var items= document.getElementsByClassName("row-items");
-    var users= document.getElementsByClassName("row-users");
-    var max_height=0;
-    for(let item of items){
-        if(item.clientHeight >max_height){
-            max_height=item.clientHeight;
-        }
-    }
-    for(let user of users){
-        if(user.clientHeight >max_height){
-            max_height=user.clientHeight;
-        }
-    }
-    for(let item of items){
-        item.setAttribute("style","height:"+max_height);
-    }
-    for(let user of users){
-        user.setAttribute("style","height:"+max_height);
-    }
-    */
-}
 showHideAdminCpButton();
 showNewNotifications();
-adjustTable();
