c466d74cbd3582598d89a072ccc4d5e2b6282689
Free-PC\Fr33LaNc3 <bugeag.george@live.com>
2018-06-14 03:47:35 +0300

    70% Done, Need to fix search bug on usereditor.

--- app/controller/admincp/AdmincpController.php
+++ app/controller/admincp/AdmincpController.php
@@ -16,8 +16,7 @@ class AdmincpController extends Controller
     {
         $this->model('Useracc');
 
-        switch($uri)
-        {
+        switch ($uri) {
             case 'admincp':
                 $this->index();
                 break;
@@ -30,7 +29,7 @@ class AdmincpController extends Controller
                 break;
             case 'admincp/dashboard':
                 $this->dashboard();
-               break;
+                break;
             case 'admincp/loginlogs':
                 $this->loginlogs();
                 break;
@@ -45,6 +44,9 @@ class AdmincpController extends Controller
                 break;
             case 'admincp/usereditor':
                 $this->usereditor();
+                break;
+            case 'admincp/usereditor/search':
+                $this->searchuser();
                 break;
             case 'admincp/usereditor/edituser':
                 $this->edituser();
@@ -77,7 +79,7 @@ class AdmincpController extends Controller
 
     private function index()
     {
-        if($_SESSION['userToEdit']) {
+        if (key_exists('userToEdit', $_SESSION)) {
             unset($_SESSION['userToEdit']);
         }
         View::CreateView(
@@ -88,22 +90,19 @@ class AdmincpController extends Controller
 
     private function login()
     {
-        if($_SESSION['userToEdit']) {
+        if (key_exists('userToEdit', $_SESSION)) {
             unset($_SESSION['userToEdit']);
         }
-        if($this->try_authenticate($_POST["uname"], $_POST["psw"], $is_admin_cp = true))
-        {
+        if ($this->try_authenticate($_POST["uname"], $_POST["psw"], $is_admin_cp = true)) {
             self::redirect('/admincp/dashboard');
-        }
-        else
-        {
+        } else {
             self::redirect('/admincp');
         }
     }
 
     private function logout()
     {
-        if($_SESSION['userToEdit']) {
+        if (key_exists('userToEdit', $_SESSION)) {
             unset($_SESSION['userToEdit']);
         }
         $this->model('UserLog');
@@ -112,8 +111,8 @@ class AdmincpController extends Controller
             'USERLOGS',
             array
             (
-                'uLogDescription'   => "'Admin user " . $_SESSION['uname']     . " has logged out!'",
-                'uLogSourceIP'      => "'" . $_SESSION['login_ip']              . "'"
+                'uLogDescription' => "'Admin user " . $_SESSION['uname'] . " has logged out!'",
+                'uLogSourceIP' => "'" . $_SESSION['login_ip'] . "'"
             )
         );
 
@@ -123,7 +122,7 @@ class AdmincpController extends Controller
 
     private function dashboard()
     {
-        if($_SESSION['userToEdit']) {
+        if (key_exists('userToEdit', $_SESSION)) {
             unset($_SESSION['userToEdit']);
         }
         View::CreateView(
@@ -133,17 +132,17 @@ class AdmincpController extends Controller
                 'totalUsers' => $this->getTotalUsers(),
                 'onlineUsers' => $this->getTotalOnline(),
                 'lastLogin' => $this->getUserLastLoginDate(),
-                'timeZone' =>  $this->getDBTimeZone(),
+                'timeZone' => $this->getDBTimeZone(),
                 'lastDBBackupTime' => $this->getLastDBBackupTime(),
                 'totalItemGroups' => $this->getTotalItemGroups(),
-                'avgItemPerGroup' =>  $this->getTotalItemGroups() ? ($this->getTotalItems()/$this->getTotalItemGroups()) : 'N/A'
+                'avgItemPerGroup' => $this->getTotalItemGroups() ? ($this->getTotalItems() / $this->getTotalItemGroups()) : 'N/A'
             ),
             'AdminCP');
     }
 
     private function itemlogs()
     {
-        if($_SESSION['userToEdit']) {
+        if (key_exists('userToEdit', $_SESSION)) {
             unset($_SESSION['userToEdit']);
         }
         View::CreateView(
@@ -154,18 +153,18 @@ class AdmincpController extends Controller
 
     private function loginlogs()
     {
-        if($_SESSION['userToEdit']) {
+        if (key_exists('userToEdit', $_SESSION)) {
             unset($_SESSION['userToEdit']);
         }
         View::CreateView(
             'admincp' . DIRECTORY_SEPARATOR . 'logs' . DIRECTORY_SEPARATOR . 'loginlogs',
-            [0=>'first',1=>'second'],
+            [0 => 'first', 1 => 'second'],
             'AdminCP');
     }
 
     private function userlogs()
     {
-        if($_SESSION['userToEdit']) {
+        if (key_exists('userToEdit', $_SESSION)) {
             unset($_SESSION['userToEdit']);
         }
         View::CreateView(
@@ -176,13 +175,12 @@ class AdmincpController extends Controller
 
     private function usereditor()
     {
-        if ($_SESSION['userToEdit']) {
+        if (key_exists('userToEdit', $_SESSION)) {
             View::CreateView(
                 'admincp' . DIRECTORY_SEPARATOR . 'users_manager' . DIRECTORY_SEPARATOR . 'editor',
                 ['userToEdit' => $_SESSION['userToEdit']],
                 'AdminCP');
-        }
-        else {
+        } else {
             View::CreateView(
                 'admincp' . DIRECTORY_SEPARATOR . 'users_manager' . DIRECTORY_SEPARATOR . 'editor',
                 [],
@@ -190,13 +188,79 @@ class AdmincpController extends Controller
         }
     }
 
-    private function goToUserEditor() {
+    private function searchuser()
+    {
+        if (isset($_POST['searchuser'])) {
+            $this->model('Useracc');
+            switch ($_POST['searchuser']) {
+                case filter_var($_POST['searchuser'], FILTER_VALIDATE_INT):
+                    $validatedfield = ' USERID = ';
+                    $user = $this->model_class->get_mapper()->findAll(
+                        $where = $validatedfield . $_POST['searchuser'],
+                        $fields = 'USERNAME, USERTYPE, USEREMAIL'
+                    );
+                    break;
+                case filter_var($_POST['searchuser'], FILTER_VALIDATE_EMAIL):
+                    $validatedfield = ' USEREMAIL = ';
+                    $user = $this->model_class->get_mapper()->findAll(
+                        $where = $validatedfield . "'" . $_POST['searchuser'] . "'",
+                        $fields = 'USERNAME, USERTYPE, USEREMAIL'
+                    );
+                    break;
+                default:
+                    $validatedfield = ' USERNAME = ';
+                    $user = $this->model_class->get_mapper()->findAll(
+                        $where = $validatedfield . "'" . $_POST['searchuser'] . "'",
+                        $fields = 'USERNAME, USERTYPE, USEREMAIL'
+                    );
+                    break;
+            }
+        }
+        else {
+            if (isset($_SESSION['userToEdit'])) {
+                unset($_SESSION['userToEdit']);
+            }
+            // Need to implement a fail message delivery .. (You need to offer some search criterias first!)
+            self::redirect('/admincp/usereditor');
+        }
+        if (empty($user) || count($user) !== 1) {
+
+//            echo var_dump($validatedfield);
+//            echo var_dump($_POST['searchuser']);
+//            echo var_dump($user);
+//            exit(0);
+            if (isset($_SESSION['userToEdit'])) {
+                unset($_SESSION['userToEdit']);
+            }
+            // Need to implement a fail message delivery .. (Couldn't find any user matching the search criteria!)
+            self::redirect('/admincp/usereditor');
+        }
+        switch ($user[0]['userType']) {
+            case 0:
+                $user[0]['userType'] = 'Unapproved';
+                break;
+            case 1:
+                $user[0]['userType'] = 'User';
+                break;
+            case 2:
+                $user[0]['userType'] = 'Root Manager';
+                break;
+            case 3:
+                $user[0]['userType'] = 'Root Admin';
+                break;
+        }
+        $_SESSION['userToEdit'] = $user[0];
+        self::redirect('/admincp/usereditor');
+    }
+
+    private function goToUserEditor()
+    {
         $this->model('Useracc');
         $user = $this->model_class->get_mapper()->findAll(
-            $where = ' USERID = '.$_POST['edituser'],
+            $where = ' USERID = ' . $_POST['edituser'],
             $fields = 'USERNAME, USERTYPE, USEREMAIL'
         );
-        switch($user[0]['userType']){
+        switch ($user[0]['userType']) {
             case 0:
                 $user[0]['userType'] = 'Unapproved';
                 break;
@@ -216,7 +280,7 @@ class AdmincpController extends Controller
 
     private function usermanager()
     {
-        if($_SESSION['userToEdit']) {
+        if (key_exists('userToEdit', $_SESSION)) {
             unset($_SESSION['userToEdit']);
         }
         View::CreateView(
@@ -227,28 +291,27 @@ class AdmincpController extends Controller
 
     private function deleteuser()
     {
-        if($_SESSION['userToEdit']) {
-            unset($_SESSION['userToEdit']);
-        }
         $this->model('Useracc');
         $user = $this->model_class->get_mapper()->findAll(
-            $where = 'USERID = '.$_POST['deleteuser']
+            $where = 'USERID = ' . $_POST['deleteuser']
         );
 
-        if ($_SESSION['userid'] === $user[0]['userId'])
-        {
+        if ($_SESSION['userid'] === $user[0]['userId']) {
             // Need to implement a fail message delivery .. (Cannot delete or suspend your own account!)
             self::redirect('/admincp/usermanager');
-        }
-        else {
+        } else {
             $query = $this->model_class->get_mapper()->delete(
                 $table = 'USERACCS',
-                $where = ' USERID = '.$_POST['edituser']
+                $where = array
+                (
+                    'USERID' => $user[0]['userId']
+                )
             );
             if ($query) {
                 $this->model('UserLog');
                 $this->model_class->get_mapper()->insert(
-                    array(
+                    $fields = array
+                    (
                         'uLogDescription' => 'Admin user ' . $_SESSION['uname'] .
                             ' has deleted user ' . $user[0]['userName'] . ' !',
                         'uLogSourceIP' => $_SESSION['login_ip']
@@ -256,9 +319,86 @@ class AdmincpController extends Controller
                 );
                 // Need to implement a success message delivery .. (User was deleted succesfully!)
                 self::redirect('/admincp/usermanager');
+            } else {
+                // Need to implement a fail message delivery .. (Could not delete user because some reasons!)
+                self::redirect('/admincp/usermanager');
             }
-            else {
-                // Need to implement a fail message delivery .. (Could not delete because some reasons!)
+        }
+    }
+
+    private function approveuser()
+    {
+        $this->model('Useracc');
+        $user = $this->model_class->get_mapper()->findAll(
+            $where = 'USERID = ' . $_POST['approveuser']
+        );
+
+        $query = $this->model_class->get_mapper()->update(
+            $table = 'USERACCS',
+            $fields = array
+            (
+                'USERSTATE' => 1
+            ),
+            $where = array
+            (
+                'USERID' => $user[0]['userid']
+            )
+        );
+        if ($query) {
+            $this->model('UserLog');
+            $this->model_class->get_mapper()->insert(
+                array
+                (
+                    'uLogDescription' => 'Admin user ' . $_SESSION['uname'] .
+                        ' has approved user ' . $user[0]['userName'] . ' !',
+                    'uLogSourceIP' => $_SESSION['login_ip']
+                )
+            );
+            // Need to implement a success message delivery .. (User was approved succesfully!)
+            self::redirect('/admincp/usermanager');
+        }
+        else {
+            // Need to implement a fail message delivery .. (Could not approve user because some reasons!)
+            self::redirect('/admincp/usermanager');
+        }
+    }
+
+    private function suspenduser()
+    {
+        $this->model('Useracc');
+        $user = $this->model_class->get_mapper()->findAll(
+            $where = 'USERID = ' . $_POST['suspenduser']
+        );
+
+        if ($_SESSION['userid'] === $user[0]['userId']) {
+            // Need to implement a fail message delivery .. (Cannot delete or suspend your own account!)
+            self::redirect('/admincp/usermanager');
+        } else {
+            $query = $this->model_class->get_mapper()->update(
+                $table = 'USERACCS',
+                $fields = array
+                (
+                    'USERSTATE' => 0
+                ),
+                $where = array
+                (
+                    'USERID' => $user[0]['userId']
+                )
+            );
+            if ($query) {
+                $this->model('UserLog');
+                $this->model_class->get_mapper()->insert(
+                    $fields = array
+                    (
+                        'uLogDescription' => 'Admin user ' . $_SESSION['uname'] .
+                            ' has suspended user ' . $user[0]['userName'] . ' !',
+                        'uLogSourceIP' => $_SESSION['login_ip']
+                    )
+                );
+                // Need to implement a success message delivery .. (User was suspended succesfully!)
+                self::redirect('/admincp/usermanager');
+            } else {
+                // Need to implement a fail message delivery .. (Could not suspend user because some reasons!)
                 self::redirect('/admincp/usermanager');
             }
         }
@@ -266,7 +406,7 @@ class AdmincpController extends Controller
 
     private function databaseeditor()
     {
-        if($_SESSION['userToEdit']) {
+        if (key_exists('userToEdit', $_SESSION)) {
             unset($_SESSION['userToEdit']);
         }
         View::CreateView(
@@ -277,7 +417,7 @@ class AdmincpController extends Controller
 
     private function settings()
     {
-        if($_SESSION['userToEdit']) {
+        if (key_exists('userToEdit', $_SESSION)) {
             unset($_SESSION['userToEdit']);
         }
         View::CreateView(
@@ -286,7 +426,8 @@ class AdmincpController extends Controller
             'AdminCP');
     }
 
-    private function getDBTimeZone() {
+    private function getDBTimeZone()
+    {
         return date_default_timezone_get();
     }
 
@@ -304,12 +445,13 @@ class AdmincpController extends Controller
         return $online_users;
     }
 
-    private function getUserLastLoginDate() {
+    private function getUserLastLoginDate()
+    {
         $this->model('Userlog');
         $queryresult =
             $this->model_class->get_mapper()->findAll(
                 $where = "ULOGDESCRIPTION like '%" . $_SESSION['uname'] . "%' "
-                    ."AND ULOGDESCRIPTION like '%has logged in%' ",
+                    . "AND ULOGDESCRIPTION like '%has logged in%' ",
                 $fields = 'to_char(ULOGCREATEDAT, \'DD-MM-YYYY HH24:MI:SS\') AS "ULOGCREATEDAT"',
                 $order = "ULOGID DESC",
                 $limit = " < 3");
@@ -318,8 +460,7 @@ class AdmincpController extends Controller
         }
         if (empty($queryresult) || count($queryresult) === 0) {
             return 'N/A';
-        }
-        else {
+        } else {
             return $queryresult[1]['uLogCreatedAt'];
         }
     }
@@ -329,47 +470,47 @@ class AdmincpController extends Controller
         $this->model('AutomatedReport');
         $dbbkuptime = $this->model_class->get_mapper()->findAll(
             $where = "REPORTTYPE = 3 "
-                ."AND REPORTFORMAT like '.xml' ",
+                . "AND REPORTFORMAT like '.xml' ",
             $fields = "TO_CHAR(\"RCREATEDAT\", 'DD-MM-YYYY HH24:MI:SS') ",
             $order = " 1 DESC",
             $limit = " = 1");
         if (count($dbbkuptime) > 1) {
             throw new RuntimeException('Something went wrong during the fetch of of last login date!');
-        }
-        elseif(empty($dbbkuptime)) {
+        } elseif (empty($dbbkuptime)) {
             return 'N/A';
-        }
-        else {
+        } else {
             return $dbbkuptime['RCREATEDAT'];
         }
     }
 
-    private function getTotalItems() {
+    private function getTotalItems()
+    {
         $this->model('Item');
         $total = $this->model_class->get_mapper()->countAll();
         return $total;
     }
 
-    private function getTotalItemGroups() {
+    private function getTotalItemGroups()
+    {
         $this->model('ItemGroup');
         $total = $this->model_class->get_mapper()->countAll();
         if ($total === 0) {
             return false;
-        }
-        else {
+        } else {
             return $total;
         }
     }
 
-    private function getActiveUserList() {
+    private function getActiveUserList()
+    {
         $this->model('Useracc');
         $query = $this->model_class->get_mapper()->findAll(
             $where = 'userType > 0',
             $fields = 'USERID, USERNAME, USERTYPE, USERSTATE, TO_CHAR(USERCREATEDAT,\'DD-MM-YYYY HH24:MI:SS\') AS "USERCREATEDAT"',
             $order = 'userCreatedAt DESC'
         );
-        for($i = 0; $i < count($query); ++$i) {
-            switch($query[$i]['userType']) {
+        for ($i = 0; $i < count($query); ++$i) {
+            switch ($query[$i]['userType']) {
                 case '1':
                     $query[$i]['userType'] = 'User';
                     break;
@@ -384,21 +525,23 @@ class AdmincpController extends Controller
         return $query;
     }
 
-    private function getUnapprovedUserList() {
+    private function getUnapprovedUserList()
+    {
         $this->model('Useracc');
         $query = $this->model_class->get_mapper()->findAll(
             $where = 'userType = 0',
             $fields = 'USERID, USERNAME, USERTYPE, USERSTATE, TO_CHAR(USERCREATEDAT,\'DD-MM-YYYY HH24:MI:SS\') AS "USERCREATEDAT"',
             $order = 'userCreatedAt DESC'
         );
-        for($i = 0; $i < count($query); ++$i) {
+        for ($i = 0; $i < count($query); ++$i) {
             $query[$i]['userType'] = 'Unapproved';
         }
         return $query;
     }
 
 
-    private function getUserLogs() {
+    private function getUserLogs()
+    {
 //        $this->model('UserLog');
 //        $query = $this->model_class->get_mapper()->findAll(
 //            $where = '',
