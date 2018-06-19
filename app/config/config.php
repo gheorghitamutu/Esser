8edbc61b99d5a2e74e0a958489455e77258d2a50
Avenger-PC\Avenger <cobzarupetrumihai96@gmail.com>
2018-06-19 20:31:18 +0300

    Fix for LOG IN

--- app/config/config.php
+++ app/config/config.php
@@ -66,3 +66,7 @@ define('SQLCMD'    , '[SQL_LOG: '           . date('d-m-Y H:i:s') . ']: ');
 // glob input macros
 define('PHP_FILE'  , '*.php');
 
+// salt
+define('SALT'      , '$1_2jlh83#@J^Q');
+define('HASH_TYPE' , 'sha512');
+
